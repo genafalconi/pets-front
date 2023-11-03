a853585a6b74b485fc6c3a5d4b017dffad4bacdc
genafalconi <genaro.f@hotmail.com>
2023-11-03 17:35:50 -0300

    fix env

--- src/helpers/errorHandler.js
+++ src/helpers/errorHandler.js
@@ -1,17 +1,35 @@
 import Swal from "sweetalert2";
+import eventBus from "./event-bus";
 
 export default function errorHandler(error) {
-  console.log(error)
-  return Swal.fire({
-    title: 'Ha ocurrido un error',
-    text: 'Sera dirigido al inicio',
-    icon: 'error',
-    timer: 2500,
-    timerProgressBar: true
-  })
-    .then(() => {
+  if (error.response && error.response.status === 401) {
+    Swal.fire({
+      title: 'Error!',
+      text: `Sesion inactiva`,
+      icon: 'error',
+      timer: 2500,
+      timerProgressBar: true,
+      showConfirmButton: false
+    }).then(() => {
       if (process.env.REACT_APP_ERROR_HANDLERS === 'true') {
         window.location.href = '/'
+        eventBus.emit('expired-sesion', true)
       }
     })
+  } else {
+    Swal.fire({
+      title: 'Error!',
+      text: `Ocurrio un error inesperado!`,
+      icon: 'error',
+      timer: 2500,
+      timerProgressBar: true,
+      showConfirmButton: false
+    })
+      .then(() => {
+        if (process.env.REACT_APP_ERROR_HANDLERS === 'true') {
+          window.location.href = '/'
+        }
+      })
+    throw new Error(error);
+  }
 }
